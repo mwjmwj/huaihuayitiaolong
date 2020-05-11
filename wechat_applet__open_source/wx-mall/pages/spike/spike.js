3db1b01414047fdfdb2cdd113b28710f0cf85286
yxg <153676508@qq.com>
2020-05-11 23:07:27 +0800

    添加链接和修改限时秒杀

--- wechat_applet__open_source/wx-mall/pages/spike/spike.js
+++ wechat_applet__open_source/wx-mall/pages/spike/spike.js
@@ -12,7 +12,15 @@ Page({
     loadmoreText: '正在加载更多数据',
     nomoreText: '全部加载完成',
     nomore: false,
-    totalPages: 1
+    totalPages: 1,
+    msTime: '23:59',
+    countdown: '',
+    countdownFlag: true,
+    day:0,
+    hou:0,
+    min:0,
+    sec:0,
+    xsList:[1,2,3,4,5,6,7,8,9]
   },
   onShareAppMessage: function () {
     return {
@@ -25,7 +33,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.countDownFun();
   },
 
   /**
@@ -118,5 +126,49 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  // 倒计时
+  countDownFun:function(){
+    var that=this;
+    var nowTime = new Date();//现在时间（时间戳）
+    var dateStr = nowTime.getFullYear()+"-" + (nowTime.getMonth() + 1) + "-" + nowTime.getDate() + " " + this.data.msTime;
+    var endTime = new Date(dateStr);//结束时间（时间戳）
+
+    var time = (endTime-nowTime)/1000;//距离结束的毫秒数
+    // 获取天、时、分、秒
+    let day = parseInt(time / (60 * 60 * 24));
+    let hou = parseInt(time % (60 * 60 * 24) / 3600);
+    let min = parseInt(time % (60 * 60 * 24) % 3600 / 60);
+    let sec = parseInt(time % (60 * 60 * 24) % 3600 % 60);
+    // console.log(day + "," + hou + "," + min + "," + sec)
+    day = that.timeFormin(day),
+    hou = that.timeFormin(hou),
+    min = that.timeFormin(min),
+    sec = that.timeFormin(sec)
+    that.setData({
+      day: that.timeFormat(day),
+      hou: that.timeFormat(hou),
+      min: that.timeFormat(min),
+      sec: that.timeFormat(sec)
+    })
+    // 每1000ms刷新一次
+    if (time>0){
+      that.setData({
+        countdownFlag: true
+      })
+      setTimeout(this.countDownFun, 1000);
+    }else{
+      that.setData({
+        countdownFlag:false
+      })
+    }
+  },
+  //小于10的格式化函数（2变成02）
+  timeFormat(param) {
+    return param < 10 ? '0' + param : param;
+  },
+  //小于0的格式化函数（不会出现负数）
+  timeFormin(param) {
+    return param < 0 ? 0: param;
   }
 })
